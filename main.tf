cd1b94b858dc657189d2d9e3810f4e87e439d0c3
nadbyra <devopsawsvideosbnk>
2024-11-28 15:30:59 +0530

    5thcommit

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws" "demo-ec2" {
+resource "aws_instance" "demo-ec2" {
   ami_id            = var.ami_id
   availability_zone = var.availability_zone
   instance_type     = var.instance_type
